af11d4d7b2d586821769e5016ae25d731aeceb26
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2024-08-27 16:11:35 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,31 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       src: '/src',
+//       components: '/src/components',
+//       pages: '/src/pages',
+//       assets: '/src/assets',
+//     },
+//   },
+//   base: '/Dragon-project/',
+// });
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      src: "/src",
-      components: "/src/components",
-      pages: "/src/pages",
-      assets: "/src/assets",
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      assets: '/src/assets',
     },
   },
-  base: "/Dragon-project/",
+  base: './',
 });
